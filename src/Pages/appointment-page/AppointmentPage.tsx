390c0062d28131145d4899461215234ded0672b5
Temiloluwa <temiloluwaOgunti8@gmail.com>
2025-04-12 18:42:00 +0100

    Working on appointment history

--- src/Pages/appointment-page/AppointmentPage.tsx
+++ src/Pages/appointment-page/AppointmentPage.tsx
@@ -1,9 +1,38 @@
 import { File, ChevronRight, Pill } from "lucide-react";
 import { Button } from "../../components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const AppointmentPage = () => {
   return (
     <div className="grid grid-cols-2 gap-4">
+      <div className="col-span-2 text-end">
+        <Sheet>
+          <SheetTrigger>
+            <Button variant="outline">View appointment history</Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Appointment history</SheetTitle>
+              <SheetDescription>
+                Summary of all your past appointments
+              </SheetDescription>
+            </SheetHeader>
+            <div>
+              w
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+      {/* Appointment details */}
       <section className="border shadow rounded-2xl col-span-2">
         <div className="flex justify-between items-center border-b py-4 px-4">
           <h2 className="font-bold">Appointment Details</h2>
@@ -57,6 +86,7 @@ const AppointmentPage = () => {
           </Button>
         </div>
       </section>
+      {/* Patient details */}
       <section className="border shadow rounded-2xl col-span-2">
         <h2 className="font-bold p-4 border-b">Patient Details</h2>
         <div className="grid gap-4 grid-cols-4 p-4">
@@ -90,30 +120,29 @@ const AppointmentPage = () => {
           </div>
         </div>
       </section>
-
       {/* Medical records */}
       <section className="border shadow rounded-2xl col-span-1">
         <h2 className="font-bold p-4 border-b">Medical records</h2>
         <div className="mx-4 my-2">
-          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100">
+          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100 cursor-pointer">
             <div className="flex gap-2 items-center">
               <File /> <span>Blood pressure report</span>
             </div>
             <ChevronRight />
           </div>
-          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100">
+          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100 cursor-pointer">
             <div className="flex gap-2 items-center">
               <File /> <span>Blood sugar level</span>
             </div>
             <ChevronRight />
           </div>
-          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100">
+          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100 cursor-pointer">
             <div className="flex gap-2 items-center">
               <File /> <span>ECG report</span>
             </div>
             <ChevronRight />
           </div>
-          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100">
+          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100 cursor-pointer">
             <div className="flex gap-2 items-center">
               <File /> <span>Cholesterol level</span>
             </div>
@@ -130,30 +159,29 @@ const AppointmentPage = () => {
           </Button>
         </div>
       </section>
-
       {/* Prescriptions */}
       <section className="border shadow rounded-2xl col-span-1">
         <h2 className="font-bold p-4 border-b">Prescriptions</h2>
         <div className="mx-4 my-2">
-          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100">
+          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100 cursor-pointer">
             <div className="flex gap-2 items-center">
               <Pill /> <span>Amlodipine 2.5mg - Morning dose</span>
             </div>
             <ChevronRight />
           </div>
-          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100">
+          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100 cursor-pointer">
             <div className="flex gap-2 items-center">
               <Pill /> <span>Metformin 500mg - Twice daily</span>
             </div>
             <ChevronRight />
           </div>
-          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100">
+          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100 cursor-pointer">
             <div className="flex gap-2 items-center">
               <Pill /> <span>Atorvastatine 10mg - Night dose</span>
             </div>
             <ChevronRight />
           </div>
-          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100">
+          <div className="border p-2 flex justify-between my-1 rounded-md bg-gray-100 cursor-pointer">
             <div className="flex gap-2 items-center">
               <Pill /> <span>Varsatan 40mg- Morning dose</span>
             </div>
