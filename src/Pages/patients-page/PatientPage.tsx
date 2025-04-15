a8e2e63b9dddba07d357a0fb887e5fa191a4760c
Temiloluwa <temiloluwaOgunti8@gmail.com>
2025-04-15 06:12:51 +0100

    Woeking on patients page

--- src/Pages/patients-page/PatientPage.tsx
+++ src/Pages/patients-page/PatientPage.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
 
 const PatientPage = () => {
   return (
     <div>
-      This is the `PatientPage` component. It is a placeholder for the actual content.
-      <p>Here you can manage patient information.</p>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-lg font-semibold">Patient Details</h2>
+        <Button variant="outline">Edit Patient</Button>
+      </div>
+      <Tabs defaultValue="overview" className="order">
+        <TabsList className="grid w-full grid-cols-5">
+          <TabsTrigger value="overview">Overview</TabsTrigger>
+          <TabsTrigger value="profile">Patient Profile</TabsTrigger>
+          <TabsTrigger value="appointments">Appointments</TabsTrigger>
+          <TabsTrigger value="treatments">Treatments</TabsTrigger>
+          {/*Treatment can be drugs, therapy or surgery */}
+          <TabsTrigger value="medical">Medical record</TabsTrigger>
+        </TabsList>
+        <TabsContent value="overview">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="profile">Change your password here.</TabsContent>
+        <TabsContent value="appointments">
+          <p>Appointments can be by drugs, therapy or surgery</p>
+        </TabsContent>
+        <TabsContent value="treatments">
+          <p>Treatments</p>
+        </TabsContent>
+        <TabsContent value="medical">
+          <p>Medical record</p>
+        </TabsContent>
+      </Tabs>
     </div>
-  )
-}
+  );
+};
 
-export default PatientPage
+export default PatientPage;
